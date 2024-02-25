d6e60cb1fc328c97dce5474f4bf1c9c6b3b1df70
Janelle <janelle.k.houston@gmail.com>
2024-02-25 16:39:49 -0500

    updated package.json

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+// const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL =
+  process.env.REACT_APP_BASE_URL || "https://jobly-backend-ijym.onrender.com";
 
 /** API Class.
  *
